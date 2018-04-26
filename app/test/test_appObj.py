2a129b238c3de6571f8a73f03f02e2e49cc46d1e
Robert Metcalf <rmetcalf9@googlemail.com>
2018-04-26 11:48:01 +0100

    Adding tests for docs

--- app/test/test_appObj.py
+++ app/test/test_appObj.py
@@ -30,3 +30,22 @@ class test_appObjClass(testHelperAPIClient):
     self.assertEqual(result.status_code, 200, msg='/ebos/AnimalsV1/swagger.json for Animals api not present')
     result = self.testClient.get('/ebodocs/AnimalsV1/swagger.json')
     self.assertEqual(result.status_code, 200, msg='/ebodocs/AnimalsV1/swagger.json for Animals apidocs not present')
+
+  @patch('githubAPICalls.githubAPICallsClass.getEBOList', return_value=[ 'AnimalsV1', 'GenderV1' ])
+  def test_docsIndexesPresent(self, getEBOListCall):
+    self.setUpMAN()
+    #Tests all locations for swagger files
+    result = self.testClient.get('/apidocs/')
+    self.assertEqual(result.status_code, 200, msg='/apidocs/ not present')
+    result = self.testClient.get('/ebodocs/GenderV1/')
+    self.assertEqual(result.status_code, 200, msg='/ebodocs/GenderV1/ not present')
+    result = self.testClient.get('/ebodocs/AnimalsV1/')
+    self.assertEqual(result.status_code, 200, msg='/ebodocs/AnimalsV1/ not present')
+    
+    #Requires trailing slash
+    #result = self.testClient.get('/apidocs')
+    #self.assertEqual(result.status_code, 200, msg='/apidocs not present')
+    #result = self.testClient.get('/ebodocs/GenderV1')
+    #self.assertEqual(result.status_code, 200, msg='/ebodocs/GenderV1 not present')
+    #result = self.testClient.get('/ebodocs/AnimalsV1')
+    #self.assertEqual(result.status_code, 200, msg='/ebodocs/AnimalsV1 not present')
